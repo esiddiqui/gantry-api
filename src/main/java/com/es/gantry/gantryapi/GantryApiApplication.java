65989c35bad5132d2040c0b2cb699304adbc6e1e
Ehtesham Siddiqui <ehtesham@bibliocommons.com>
2018-02-03 13:43:19 -0500

    Initial project setup

---
+++ src/main/java/com/es/gantry/gantryapi/GantryApiApplication.java
@@ -0,0 +1,12 @@
+package com.es.gantry.gantryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GantryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GantryApiApplication.class, args);
+	}
+}
